64d0775306de3a5cf303ff8116f99406f5241d80
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-07-27 22:49:59 -0500

    Update PathPackager.lua

--- PathPackager.lua
+++ PathPackager.lua
@@ -32,3 +32,47 @@ function RoomPackager:TopCornersOfBasePlate(basePlate)
 	end
 	return topCorners
 end
+
+function RoomPackager:RegionsFromBasePlate(basePlate)
+	local topCorners = self:TopCornersOfBasePlate(basePlate)
+	local arbitraryCorner = topCorners[1]
+	local minX = arbitraryCorner.X
+	local minZ = arbitraryCorner.Z
+	local maxX = arbitraryCorner.X
+	local maxZ = arbitraryCorner.Z
+	for _, corner in pairs(topCorners) do
+		minX = math.min(minX, corner.X)
+		minZ = math.min(minZ, corner.Z)
+		maxX = math.max(maxX, corner.X)
+		maxZ = math.max(maxZ, corner.Z)
+	end
+	local minY = topCorners[1].Y
+	local lowerCorner = Vector3.new(minX, minY, minZ)
+	local maxY = minY + 70
+	local upperCorner = Vector3.new(maxX, maxY, maxZ)
+
+	local segmentHeight = math.floor(100000/(math.abs(maxX-minX)*math.abs(maxZ-minZ)))
+
+	local regions = {}
+
+	local currentHeight = minY
+	while currentHeight - minY < 70 do
+		currentHeight = currentHeight + segmentHeight
+		lowerCorner = Vector3.new(lowerCorner.x, currentHeight - segmentHeight, lowerCorner.z)
+		upperCorner = Vector3.new(upperCorner.x, currentHeight, upperCorner.z)
+		table.insert(regions, Region3.new(lowerCorner, upperCorner))
+	end
+
+	return regions
+end
+
+local function closestParentToWorkspace(object, roomModel)
+	if object.Parent == roomModel then
+		return nil
+	end
+	if object.Parent == game.Workspace then
+		return object
+	else
+		return closestParentToWorkspace(object.Parent, roomModel)
+	end
+end
